2f7def421d713bf1969a6b3924c7f281259575f7
Andrew Pierce <andrew.pierce@red-gate.com>
2020-02-21 12:04:35 -0800

    fixed script

--- Widget_Hybrid/Migrations/1.1.0-Changes/006_20200204-1139_andrew.pierce.sql
+++ Widget_Hybrid/Migrations/1.1.0-Changes/006_20200204-1139_andrew.pierce.sql
@@ -16,16 +16,3 @@ GO
 ALTER TABLE [dbo].[customersDemo] ADD
 [AzureUsername] [nvarchar] (200) NULL
 GO
-
-INSERT INTO dbo.PersonTest(PersonId, PersonName, PersonDOB, PersonAddress, EmailAddress, PersonState, CountryCode, LastModified, LastModifiedBy, Address1)
-VALUES(0, -- PersonId - bigint
-N'Nana' , -- PersonName - nvarchar(40)
-GETDATE(), -- PersonDOB - date
-N'337 Monor ave.' , -- PersonAddress - nvarchar(50)
-N'Hello@Gmail.com' , -- EmailAddress - nvarchar(50)
-N'CA' , -- PersonState - nchar(2)
-N'USA' , -- CountryCode - nchar(2)
-SYSDATETIME(), -- LastModified - datetime2(7)
-N'Bob' , -- LastModifiedBy - nvarchar(50)
-N'Pasadena' -- Address1 - nvarchar(50)
-    )
